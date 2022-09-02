0014afe0b46372e9dae8189d8f8ce12b15fe349a
sinotca529-wsl <52117877+sinotca529@users.noreply.github.com>
2022-09-02 10:19:45 +0900

    add a parameter max_sw

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A neovim plugin to adjust indent type automatically.
 This plugin adjusts `expandtab` and `shiftwidth` as suggested by buffer.
 
 If there is a line beginning with spaces, set `expandtab` as `true` and set `shiftwidth` as the number of spaces.
-If there is a line beginnign with tab, set `expandtab` as `false`.
+If there is a line beginning with tab, set `expandtab` as `false`.
 Otherwise, do nothing.
 
 ## Requirements
@@ -18,4 +18,20 @@ Using packer:
 use {'sinotca529/adjust-indent.nvim'}
 require('adjust-indent').setup()
 ```
+
+## Settings
+### Example
+```lua
+use {'sinotca529/adjust-indent.nvim'}
+require('adjust-indent').setup({
+    max_sw = 4,
+})
+```
+
+### Config
+#### `max_sw`
+The parameter sets max `shiftwidth`.
+Any lines in which the number of leading spaces is more than `max_sw` are ignored when detecting indent width.
+
+(default is 8)
 
--- lua/adjust-indent.lua
+++ lua/adjust-indent.lua
@@ -1,24 +1,42 @@
-local setup = function()
+local set_config = function(user_conf)
+    local default = {
+        max_sw = 8, -- max shift width
+    }
+
+    user_conf = user_conf or {}
+    return vim.tbl_deep_extend('force', default, user_conf)
+end
+
+local main = function(user_conf)
+    local conf = set_config(user_conf)
+
+    -- CASE : Indent is space(s).
+    local line_num = vim.fn.search(string.format(
+        '^ \\{1, %q}',
+        conf.max_sw
+    ))
+    if line_num ~= 0 then
+        local line = vim.fn.getline(line_num)
+        local _, e = string.find(line, [[^ +]])
+        vim.opt_local.expandtab = true;
+        vim.opt_local.shiftwidth = e;
+        return
+    end
+
+    -- CASE : Indent is tab.
+    local tab_line_num = vim.fn.search([[^\t\+]])
+    if tab_line_num ~= 0 then
+        vim.opt_local.expandtab = false;
+        return
+    end
+end
+
+local setup = function(user_conf)
     vim.api.nvim_create_augroup('adjust-indent', {})
     vim.api.nvim_create_autocmd('BufRead', {
         group = 'adjust-indent',
         callback = function()
-            -- CASE : Indent is space(s).
-            local line_num = vim.fn.search([[^ \+]])
-            if line_num ~= 0 then
-                local line = vim.fn.getline(line_num)
-                local _, e = string.find(line, [[^ +]])
-                vim.opt_local.expandtab = true;
-                vim.opt_local.shiftwidth = e;
-                return
-            end
-
-            -- CASE : Indent is tab.
-            local tab_line_num = vim.fn.search([[^\t\+]])
-            if tab_line_num ~= 0 then
-                vim.opt_local.expandtab = false;
-                return
-            end
+            main(user_conf)
         end
     })
 end
